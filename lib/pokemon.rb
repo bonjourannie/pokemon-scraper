92a6ed2ab7427d9c16814bdd596ace7536e72879
Annie Truex <artruex@gmail.com>
2019-10-09 18:24:51 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,12 @@
 class Pokemon
   
-  attr_accessor :id, :name, :type 
+  attr_accessor :id, :name, :type, :db 
+  
+  def initialize(id=nil, name, type, db)
+    @id = id 
+    @name = name 
+    @type = type 
+    @db = db
+  end
   
 end
